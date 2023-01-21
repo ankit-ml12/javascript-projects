097d063d0507e11e18a8370b7c0d3a2df3fdb3e7
ankit patidar <patiankit6555@gmail.com>
2023-01-21 22:23:49 +0530

    21/01 1

--- 02 Model/script.js
+++ 02 Model/script.js
@@ -1,1 +1,31 @@
-'use strict';
+"use strict";
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModel = document.querySelector(".close-modal");
+const btnOpenModel = document.querySelectorAll(".show-modal");
+// console.log(modal);
+// console.log(overlay);
+// console.log(btnCloseModel);
+// console.log(btnOpenModel);
+
+for (let i = 0; i < btnOpenModel.length; i++) {
+  btnOpenModel[i].addEventListener("click", function () {
+    modal.classList.remove("hidden");
+    overlay.classList.remove("hidden");
+  });
+}
+let closeModel = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnCloseModel.addEventListener("click", closeModel);
+overlay.addEventListener("click", closeModel);
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    if (!modal.classList.contains("hidden")) {
+      closeModel();
+    }
+  }
+});
