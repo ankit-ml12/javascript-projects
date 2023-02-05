4cdb3e16b181de551fb98aa01efcdb6ccd4f3677
ankit patidar <patiankit6555@gmail.com>
2023-02-05 17:25:06 +0530

    navigation

--- 05 bankist marketing/script.js
+++ 05 bankist marketing/script.js
@@ -30,6 +30,43 @@ document.addEventListener("keydown", function (e) {
   }
 });
 
+///////////////////////////////////////////////////
+// page navigation
+
+// document.querySelectorAll(".nav__link").forEach(function (el) {
+//   el.addEventListener("click", function (e) {
+//     e.preventDefault();
+//     console.log("hello ankit");
+//     const id = this.getAttribute("href");
+
+//     document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+//   });
+// });
+
+//it is ok for three button but let suppose if we have 1000 button then it is not good practice
+// to add 1000 even listner or may be million
+//which impect our performance
+
+//so we put even handller on grand parent and user click on the that element then even is generated
+//and bubbles up and then we catch the event because we also know where the event originated
+// by looking at event.target property
+
+//==> event delegation steps
+//1. add event to the common parent element
+//2. determine what element originated the event
+
+document.querySelector(".nav__links").addEventListener("click", function (e) {
+  //and that event store inthe e.target
+  console.log(e.target);
+  e.preventDefault();
+  //apply matching stretegy
+  if (e.target.classList.contains("nav__link")) {
+    const id = e.target.getAttribute("href");
+    document.querySelector(id).scrollIntoView({ behavior: "smooth" });
+  }
+});
+
+//even delegation is more efficient and effective
 /*
 
 // selecting
@@ -183,9 +220,9 @@ const h1 = document.querySelector("h1");
 //1. you can set multiple event in case of old second event override the first
 //2. you can remove it if not required
 
-const temp = function () {
-  alert("hello testing with ankit");
-  h1.removeEventListener("mouseenter", temp);
-};
-h1.addEventListener("mouseenter", temp);
+// const temp = function () {
+//   alert("hello testing with ankit");
+//   h1.removeEventListener("mouseenter", temp);
+// };
+// h1.addEventListener("mouseenter", temp);
 // we can also remove using setTimeOut function
